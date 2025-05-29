06e55f3c188bfc74ab6da34a06a37343de446713
Markame <mr.markame@gmail.com>
2025-05-29 14:48:05 -0300

    novocode

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -7,13 +7,30 @@
         {
             InitializeComponent();
         }
-
+        
         public void Salvar(object sender, EventArgs e)
         {
-
-            DisplayAlert("Item salvo",
-                "O cadastro de  foi salvo",
+            var _nome = nome.Text;
+            var _cpf = cpf.Text;
+            var _endereco = endereco.Text; 
+            var _senha = senha.Text;
+           
+            if (string.IsNullOrEmpty(_nome)
+                && string.IsNullOrEmpty(_cpf) &&
+                string.IsNullOrEmpty(_senha)) { 
+            DisplayAlert("Item faltante",
+                "Algum item não foi preenchido",
                 "ok");
+            }
+            else { 
+            DisplayAlert("Item salvo",
+                "O cadastro " + _nome + " foi salvo",
+                "ok");
+                Nome.Text = _nome;
+                CPF.Text = _cpf;
+                Endereco.Text = _endereco;
+                Senha.Text = _senha;
+            }
             
         }    
         public async void Limpar(object sender, EventArgs e)
@@ -23,6 +40,10 @@
                 "sim","não");
             if (resposta)
             {
+                nome.Text = "";
+                cpf.Text = "";
+                endereco.Text = "";
+                senha.Text = "";
                
                 await DisplayAlert("Apagou","Tudo foi apagado","OK");
             }
