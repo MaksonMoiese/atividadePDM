0650595f55795e56fb50efdef739ba44f6c2c785
Markame <mr.markame@gmail.com>
2025-05-29 07:18:21 -0300

    29052025

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -10,18 +10,21 @@
 
         public void Salvar(object sender, EventArgs e)
         {
-            DisplayAlert("Tudo ok",
-                "Formulário salvo com sucesso",
+
+            DisplayAlert("Item salvo",
+                "O cadastro de  foi salvo",
                 "ok");
+            
         }    
         public async void Limpar(object sender, EventArgs e)
         {
             var resposta = await DisplayAlert("Tem Certeza?",
-                "Todo o conteudo do formulario será perdido",
+                "Todo o conteúdo do formulário será perdido",
                 "sim","não");
             if (resposta)
             {
-                DisplayAlert("Apagou","Tudo foi apagado","OK");
+               
+                await DisplayAlert("Apagou","Tudo foi apagado","OK");
             }
         }
 
